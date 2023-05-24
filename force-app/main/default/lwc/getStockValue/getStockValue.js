4046be675c817d1ff1af5fa5faf61517297f04d9
Ankur Trivedi <anktridev@gmail.com>
2023-05-24 21:58:27 +0530

    Salesforce Integration

---
+++ force-app/main/default/lwc/getStockValue/getStockValue.js
@@ -0,0 +1,14 @@
+import { LightningElement } from "lwc";
+import FetchStock from "@salesforce/apex/AlphaVantageController.FetchStock";
+export default class GetStockValue extends LightningElement {
+    connectedCallback() {
+        this.FetchData();
+    }
+    FetchData() {
+        FetchStock('MSFT').then(response => {
+            console.log('Data--', response);
+        }).catch(error => {
+            console.error('Not working', error.body);
+        });
+    }
+}
