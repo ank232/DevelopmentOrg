12eef7ea5446c7308faebb91f190e0bbf8f4b76c
Ankur Trivedi <anktridev@gmail.com>
2023-06-16 01:55:16 +0530

    created customerdetails contoller

--- force-app/main/default/lwc/createInvoiceFromAccount/createInvoiceFromAccount.js
+++ force-app/main/default/lwc/createInvoiceFromAccount/createInvoiceFromAccount.js
@@ -1,6 +1,7 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 import INVOICE_OBJECT from '@salesforce/schema/Invoice__c';
+import { getRecord } from 'lightning/uiRecordApi';
 // import INVOICE_COMPANY_NAME_FIELD from '@salesforce/schema/Invoice__c.Company__c';
 // import INVOICE_CUSTOMER_NAME_FIELD from '@salesforce/schema/Invoice__c.Customer__c';
 // import INVOICE_STATUS_FIELD from '@salesforce/schema/Invoice__c.Status__c';
@@ -11,18 +12,37 @@ import INVOICE_OBJECT from '@salesforce/schema/Invoice__c';
 // import INVOICE_REFERENCE_FIELD from '@salesforce/schema/Invoice__c.Reference__c';
 export default class CreateInvoiceFromAccount extends LightningElement {
     /* ------- Proprties-----------  */
-    @track customerName;
-    @track invoiceStatus;
-    @track invoiceDate;
-    @track invoiceDueDate;
-    @track invoicePaidDate;
-    @track invoiceNumber;
+    customerName;
+    invoiceStatus;
+    invoiceDate;
+    invoiceDueDate;
+    nvoicePaidDate;
+    invoiceNumber;
     objectApiName = INVOICE_OBJECT;
+    customerId;
+    customerData = {};
+    // @wire(getRecord, { recordId: '$customerId', fields: ['Customer__r.Name, Customer__r.Email'] })
+    // customerRecord({ error, data }) {
+    //     if (data) {
+    //         this.customerData = data.fields;
+    //     } else if (error) {
 
+    //     }
+    // }
+    handleCustomerDetail(event) {
+        const recordtest = event.detail.records[this.objectApiName];
+        this.customerId = recordtest.fields.Customer__c.value;
+        console.log('-- -- --');
+        console.log(this.customerId);
+    }
     GenerateInvoice(event) {
         event.preventDefault();
-        const inputFields = this.template.querySelector('lightning-input-field');
-        console.log('Fields Entered--', inputFields);
+        const formInputs = this.template.querySelectorAll('lightning-input-field');
+        formInputs.forEach(inputfield => {
+            console.log('FieldName=');
+            console.log(inputfield.fieldName);
+            console.log(inputfield.value);
+        });
     }
 
     // @wire(getPicklistValues, {
