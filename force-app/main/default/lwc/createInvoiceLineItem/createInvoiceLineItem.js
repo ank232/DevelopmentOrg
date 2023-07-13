cfc97882a661cbe00add16b59d96df62b1a368d5
Ankur Trivedi <anktridev@gmail.com>
2023-07-14 00:56:34 +0530

    working on InvoiceLineItem lwc and search is complete

--- force-app/main/default/lwc/createInvoiceLineItem/createInvoiceLineItem.js
+++ force-app/main/default/lwc/createInvoiceLineItem/createInvoiceLineItem.js
@@ -1,23 +1,28 @@
-import { LightningElement } from 'lwc';
-import { createRecord } from 'lightning/uiRecordApi';
-import INVOICE_NO_FIELD from '@salesforce/schema/Invoice_Line_Items__c.Invoice__c'
-import PRODUCT_NAME_FIELD from '@salesforce/schema/Invoice_Line_Items__c.Product__c';
-import QUANTITY_FIELD from '@salesforce/schema/Invoice_Line_Items__c.Quantity__c';
-import TAX_FIELD from '@salesforce/schema/Invoice_Line_Items__c.Tax__c';
+import { LightningElement, wire } from 'lwc';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 import TAX_TYPE_FIELD from '@salesforce/schema/Invoice_Line_Items__c.Tax_Type__c';
-import UNIT_AMOUNT_FIELD from '@salesforce/schema/Invoice_Line_Items__c.Unit_Amount__c';
-const COLS = [
-    { label: 'Product Name', fieldName: 'Product__c', type: 'text', editable: true }
-];
 export default class CreateInvoiceLineItem extends LightningElement {
     keyIndex = 0;
+    TaxPicklistVals = [];
     lineItems = [{
         Id: '1',
-        Product__c: '',
-        Quantity__c: '',
-        Unit_Amount__c: '',
-        Tax_Type__c: '',
-        Tax_Amount__c: ''
+        ProductName: '',
+        Quantity: '',
+        Description: '',
+        UnitAmount: '',
+        TaxPercent: ''
     }];
-
+    @wire(getPicklistValues, { fieldApiName: TAX_TYPE_FIELD })
+    LineItemPickListVals({ data, error }) {
+        if (data) {
+            console.log('-----------------');
+            console.log(data);
+        }
+        if (error) {
+            console.log('XXXXXXXXXXXXXXXXXXXX');
+        }
+    }
+    connectedCallback() {
+        console.log(TAX_TYPE_FIELD);
+    }
 }
---
+++ force-app/main/default/lwc/productSearch/productSearch.js
@@ -0,0 +1,55 @@
+import { LightningElement } from 'lwc';
+import productSearch from '@salesforce/apex/CustomerDetailsController.productSearch';
+export default class ProductSearch extends LightningElement {
+    /****************  Properties  ************/
+    searchProductName;
+    searchResults;
+    showProd; // bool
+    /*Method to Search Product*/
+    SearchQuery(prodName) {
+            const searchTerm = prodName;
+            productSearch({ ProductName: searchTerm })
+                .then(result => {
+                    if (result.length == 0) {
+                        console.log('No product found');
+                        this.searchResults = null;
+                    } else {
+                        console.log('Found----');
+                        console.log(result);
+                        this.searchResults = result;
+                    }
+                }).catch(error => {
+                    console.log('!!!Error!!!');
+                    console.log(error);
+                })
+            return this.searchResults;
+        }
+        /*
+        Purpose: to handle user Input
+        event -> Onchange()
+         */
+    handleProductSearch = (event) => {
+            this.searchProductName = event.target.value;
+            if (!this.searchProductName) {
+                console.log('You searched Nothing.....');
+                this.showProd = false;
+                return;
+            }
+            const res = this.SearchQuery(this.searchProductName);
+            if (!res) {
+                this.showProd = false;
+            }
+            this.showProd = true;
+        }
+        /*
+        Purpose: to get the selected productId
+        event -> onClick()
+        */
+    ProductSelection = (event) => {
+        console.log('You selected a product');
+        console.log(event.target.innerText);
+        this.searchProductName = event.target.innerText;
+        this.showProd = false;
+        console.log(event.target.dataset.product);
+    }
+}
