3bb7d12e9b2f635c7669f5cc2679e97a840d5e5d
Ankur Trivedi <anktridev@gmail.com>
2023-07-14 15:18:54 +0530

    added dynamic row addition funcitionality

--- force-app/main/default/lwc/createInvoiceLineItem/createInvoiceLineItem.js
+++ force-app/main/default/lwc/createInvoiceLineItem/createInvoiceLineItem.js
@@ -1,9 +1,10 @@
 import { LightningElement, wire } from 'lwc';
 import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import TAX_TYPE_FIELD from '@salesforce/schema/Invoice_Line_Items__c.Tax_Type__c';
+import INVOICE_LINE_ITEM from '@salesforce/schema/Invoice_Line_Items__c';
 export default class CreateInvoiceLineItem extends LightningElement {
     keyIndex = 0;
-    TaxPicklistVals = [];
     lineItems = [{
         Id: '1',
         ProductName: '',
@@ -12,17 +13,62 @@ export default class CreateInvoiceLineItem extends LightningElement {
         UnitAmount: '',
         TaxPercent: ''
     }];
-    @wire(getPicklistValues, { fieldApiName: TAX_TYPE_FIELD })
-    LineItemPickListVals({ data, error }) {
-        if (data) {
-            console.log('-----------------');
-            console.log(data);
+
+    @wire(getObjectInfo, { objectApiName: INVOICE_LINE_ITEM })
+    objectInfo;
+
+    @wire(getPicklistValues, {
+        recordTypeId: '$objectInfo.data.defaultRecordTypeId',
+        fieldApiName: TAX_TYPE_FIELD
+    })
+    taxPicklistValues;
+
+    get TaxPicklistVals() {
+        if (this.taxPicklistValues.data) {
+            return this.taxPicklistValues.data.values.map(picklist => ({
+                label: picklist.label,
+                value: picklist.value
+            }));
         }
-        if (error) {
-            console.log('XXXXXXXXXXXXXXXXXXXX');
-        }
+        return [];
     }
-    connectedCallback() {
-        console.log(TAX_TYPE_FIELD);
+    userInput() {
+            const template = this.template.querySelectorAll('lightning-input');
+            console.log(template);
+        }
+        // Event Handlers:
+    handleDescription = (event) => {
+        console.log("^^^^&&&&&^^^^");
+        console.log(event.target.value);
+    }
+    handleQuantity = (event) => {
+        console.log("^^^^&&&&&^^^^");
+        console.log(event.target.value);
+    }
+    handleunitAmount = (event) => {
+        console.log("^^^^&&&&&^^^^");
+        console.log(event.target.value);
+    }
+    handleTaxType = (event) => {
+        console.log("^^^^&&&&&^^^^");
+        console.log(event.target.value);
+    }
+    handleTaxInput = (event) => {
+        console.log("^^^^&&&&&^^^^");
+        console.log(event.target.value);
+    }
+    handleDeleteLineItem = (event) => {
+        this.userInput();
+    }
+    AddLineItem = () => {
+        console.log('I clickeed!!!');
+        const newItem = {
+            Id: String(this.lineItems.length + 1),
+            ProductName: '',
+            Quantity: '',
+            UnitAmount: '',
+            TaxPercent: ''
+        };
+        this.lineItems = [...this.lineItems, newItem];
     }
 }
