443f7ee5da8c10f66ad4bf4d83a39bf88cd2fb8a
Ankur Trivedi <anktridev@gmail.com>
2023-07-15 23:40:46 +0530

    row deletion completed

--- force-app/main/default/lwc/createInvoiceLineItem/createInvoiceLineItem.js
+++ force-app/main/default/lwc/createInvoiceLineItem/createInvoiceLineItem.js
@@ -1,8 +1,10 @@
 import { LightningElement, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import TAX_TYPE_FIELD from '@salesforce/schema/Invoice_Line_Items__c.Tax_Type__c';
 import INVOICE_LINE_ITEM from '@salesforce/schema/Invoice_Line_Items__c';
+import EmailPreferencesStayInTouchReminder from '@salesforce/schema/User.EmailPreferencesStayInTouchReminder';
 export default class CreateInvoiceLineItem extends LightningElement {
     keyIndex = 0;
     lineItems = [{
@@ -33,18 +35,23 @@ export default class CreateInvoiceLineItem extends LightningElement {
         }
         return [];
     }
-    userInput() {
-            const template = this.template.querySelectorAll('lightning-input');
-            console.log(template);
+    showNoficiation(title, message, variant) {
+            const showToast = new ShowToastEvent({
+                title: title,
+                message: message,
+                variant: variant
+            });
+            this.dispatchEvent(showToast);
         }
         // Event Handlers:
     handleProductName = (event) => {
         console.log('Row is=====');
-        console.log(event.target.dataset.id);
+        const rowId = event.target.dataset.id;
         console.log('Data recieved from Child---');
         console.log(JSON.stringify(event.detail));
         const prodName = event.detail.productName;
         console.log(prodName);
+        this.lineItems[rowId]['ProductName'] = prodName;
     }
     handleDescription = (event) => {
         console.log(event.target.value);
@@ -71,9 +78,15 @@ export default class CreateInvoiceLineItem extends LightningElement {
     handleDeleteLineItem = (event) => {
         console.log('I will delete the row');
         const rowIndex = event.target.dataset.rowIndex;
-        console.log("YOu selected---------", rowIndex);
+        console.log("YOu selected---------row==>", rowIndex);
         console.log('Related Data:');
         console.log(JSON.stringify(this.lineItems[rowIndex]));
+        if (this.lineItems.length > 1) {
+            this.lineItems.splice(rowIndex, 1);
+            this.lineItems = [...this.lineItems];
+        } else {
+            this.showNoficiation('Warning', "Cannot delete all rows", "Warning");
+        }
     }
     AddLineItem = () => {
         console.log('I clickeed!!!');
@@ -89,6 +102,18 @@ export default class CreateInvoiceLineItem extends LightningElement {
     }
     SaveLineItem = () => {
         console.log('I will save the data');
-        console.log(JSON.stringify(this.lineItems));
+        this.lineItems.map((item) => {
+            if (!item.Description) {
+                console.log('Error at row---> ' + item.Id);
+                this.showNoficiation(
+                    "Error",
+                    "Item at" + item.Id + "Field Cannot be left empty ",
+                    "Error"
+                );
+                return;
+            } else {
+                this.showNoficiation("Sucess", "I will create the line Items", "Success");
+            }
+        });
     }
 }
