0a75bd73e13f9aa6de4550b63905067c3c6ed247
Ankur Trivedi <anktridev@gmail.com>
2023-07-16 18:43:06 +0530

    completed lineitem lwc

--- force-app/main/default/lwc/createInvoiceLineItem/createInvoiceLineItem.js
+++ force-app/main/default/lwc/createInvoiceLineItem/createInvoiceLineItem.js
@@ -6,6 +6,8 @@ import TAX_TYPE_FIELD from '@salesforce/schema/Invoice_Line_Items__c.Tax_Type__c
 import INVOICE_LINE_ITEM from '@salesforce/schema/Invoice_Line_Items__c';
 import EmailPreferencesStayInTouchReminder from '@salesforce/schema/User.EmailPreferencesStayInTouchReminder';
 export default class CreateInvoiceLineItem extends LightningElement {
+    taxAmount;
+    totalAmount
     keyIndex = 0;
     lineItems = [{
         Id: '1',
@@ -45,7 +47,6 @@ export default class CreateInvoiceLineItem extends LightningElement {
         }
         // Event Handlers:
     handleProductName = (event) => {
-        console.log('Row is=====');
         const rowId = event.target.dataset.id;
         console.log('Data recieved from Child---');
         console.log(JSON.stringify(event.detail));
@@ -54,33 +55,30 @@ export default class CreateInvoiceLineItem extends LightningElement {
         this.lineItems[rowId]['ProductName'] = prodName;
     }
     handleDescription = (event) => {
-        console.log(event.target.value);
         const row = event.target.dataset.id;
-        console.log(row);
         this.lineItems[row]['Description'] = event.target.value;
     }
     handleQuantity = (event) => {
-        console.log(event.target.value);
+        const row = event.target.dataset.id;
+        this.lineItems[row]['Quantity'] = event.target.value;
+        this.CalculateTaxAmount(row);
     }
     handleunitAmount = (event) => {
-        console.log(event.target.value);
+        const row = event.target.dataset.id;
+        this.lineItems[row]['UnitAmount'] = event.target.value;
+        this.CalculateTaxAmount(row);
     }
     handleTaxType = (event) => {
-        console.log(event.target.value);
         const row = event.target.dataset.id;
-        console.log('Row----');
-        console.log(row);
         this.lineItems[row]['TaxType'] = event.target.value;
     }
     handleTaxInput = (event) => {
-        console.log(event.target.value);
+        const row = event.target.dataset.id;
+        this.lineItems[row]['TaxPercent'] = event.target.value;
+        this.CalculateTaxAmount(row);
     }
     handleDeleteLineItem = (event) => {
-        console.log('I will delete the row');
         const rowIndex = event.target.dataset.rowIndex;
-        console.log("YOu selected---------row==>", rowIndex);
-        console.log('Related Data:');
-        console.log(JSON.stringify(this.lineItems[rowIndex]));
         if (this.lineItems.length > 1) {
             this.lineItems.splice(rowIndex, 1);
             this.lineItems = [...this.lineItems];
@@ -89,7 +87,6 @@ export default class CreateInvoiceLineItem extends LightningElement {
         }
     }
     AddLineItem = () => {
-        console.log('I clickeed!!!');
         const newItem = {
             Id: String(this.lineItems.length + 1),
             ProductName: '',
@@ -101,19 +98,34 @@ export default class CreateInvoiceLineItem extends LightningElement {
         this.lineItems = [...this.lineItems, newItem];
     }
     SaveLineItem = () => {
-        console.log('I will save the data');
         this.lineItems.map((item) => {
             if (!item.Description) {
                 console.log('Error at row---> ' + item.Id);
                 this.showNoficiation(
                     "Error",
-                    "Item at" + item.Id + "Field Cannot be left empty ",
+                    `Item at Row${item.Id}Field Cannot be left empty `,
                     "Error"
                 );
                 return;
             } else {
+                console.log("VVVVVV");
+                console.log(JSON.stringify(item));
                 this.showNoficiation("Sucess", "I will create the line Items", "Success");
             }
         });
+    }
+    CalculateTaxAmount(row) {
+        const unitAmount = parseFloat(this.lineItems[row]["UnitAmount"]);
+        const quantity = parseFloat(this.lineItems[row]["Quantity"]);
+        const taxperecnt = parseFloat(this.lineItems[row]["TaxPercent"]);
+        if (!isNaN(unitAmount) && !isNaN(quantity) && !isNaN(taxperecnt)) {
+            const calcluatedTax = (unitAmount * (taxperecnt / 100)) * quantity;
+            this.taxAmount = calcluatedTax.toFixed(2);
+            const calclulatedAmount = unitAmount * quantity;
+            this.totalAmount = calclulatedAmount;
+        } else {
+            this.taxAmount = '';
+            this.totalAmount = '';
+        }
     }
 }
