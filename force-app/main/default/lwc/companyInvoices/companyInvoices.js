2621c9fff183765c556a7a209157ed39a1af8009
Ankur Trivedi <anktridev@gmail.com>
2023-07-23 02:32:00 +0530

    row action added to datatable lwc

--- force-app/main/default/lwc/companyInvoices/companyInvoices.js
+++ force-app/main/default/lwc/companyInvoices/companyInvoices.js
@@ -1,13 +1,21 @@
 import { LightningElement, wire, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import ShowInvoices from '@salesforce/apex/CustomerDetailsController.ShowInvoices';
 import Status_FIELD from '@salesforce/schema/Invoice__c.Status__c';
 import INVOICE_DATE from '@salesforce/schema/Invoice__c.Invoice_Date__c';
 import Line_AMOUNT_FIELD from '@salesforce/schema/Invoice__c.Total_Line_Amount__c';
 import DUE_DATE from '@salesforce/schema/Invoice__c.Due_Date__c';
 import INVOICE_NO_FIELD from '@salesforce/schema/Invoice__c.Name';
+const actions = [
+    { label: 'View', name: 'view' },
+    { label: 'Delete', name: 'delete' },
+];
 const COLS = [{
         label: 'Invoice No',
-        fieldName: INVOICE_NO_FIELD.fieldApiName
+        fieldName: 'InvoiceNO',
+        type: 'url',
+        typeAttributes: { label: { fieldName: INVOICE_NO_FIELD.fieldApiName }, target: '_blank' }
     },
     {
         label: 'Customer Name',
@@ -21,20 +29,29 @@ const COLS = [{
     },
     {
         label: 'Total Line Amount',
-        fieldName: Line_AMOUNT_FIELD.fieldApiName
+        fieldName: Line_AMOUNT_FIELD.fieldApiName,
+        type: 'currency'
     },
     {
         label: 'Invoice Date',
-        fieldName: INVOICE_DATE.fieldApiName
+        fieldName: INVOICE_DATE.fieldApiName,
+        type: 'date'
     }, {
         label: 'Due Date',
-        fieldName: DUE_DATE.fieldApiName
+        fieldName: DUE_DATE.fieldApiName,
+        type: 'date'
+    },
+    {
+        // label: 'Action',
+        type: 'action',
+        initialWidth: '50px',
+        typeAttributes: { rowActions: actions },
     }
 ];
 /*
 This Component will show the related invoices for the company
 */
-export default class CompanyInvoices extends LightningElement {
+export default class CompanyInvoices extends NavigationMixin(LightningElement) {
     @api recordId;
     columns = COLS;
     invoiceData = [];
@@ -46,10 +63,13 @@ export default class CompanyInvoices extends LightningElement {
             if (data.length == 0) {
                 this.invoiceData = null;
             } else {
+
                 this.invoiceData = data.map((invoice) => {
                     const customerName = invoice.Customer__r ? `${invoice.Customer__r.FirstName} ${invoice.Customer__r.LastName}` : 'N/A';
+                    const InvoiceNO = `/${invoice.Id}`;
                     return {
                         ...invoice,
+                        InvoiceNO,
                         CustomerName: customerName
                     }
                 });
@@ -60,5 +80,34 @@ export default class CompanyInvoices extends LightningElement {
             console.log("--- ------ -----  -----");
             console.log(JSON.stringify(error));
         }
+    }
+    Rowmenu = (event) => {
+        console.log('You selected a row!!');
+        const row = event.detail.row;
+        const actionName = event.detail.action.name;
+        console.log(JSON.stringify(row), '---Action---> ', actionName);
+        switch (actionName) {
+            case 'view':
+                this[NavigationMixin.Navigate]({
+                    type: 'standard__recordPage',
+                    attributes: {
+                        recordId: row.Id,
+                        actionName: 'view'
+                    }
+                });
+                break;
+            case 'delete':
+                this.showNoficiation("Message", "Invoice " + row.Name + " is deleted", "Message");
+                break;
+            default:
+        }
+    }
+    showNoficiation(title, message, variant) {
+        const showToast = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(showToast);
     }
 }
