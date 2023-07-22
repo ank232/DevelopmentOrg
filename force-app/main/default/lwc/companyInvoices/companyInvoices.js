dc7bc8435b944205441193fe9d9f35ce513db74c
Ankur Trivedi <anktridev@gmail.com>
2023-07-22 20:19:40 +0530

    added extra condition to datatable in invoices LWC

--- force-app/main/default/lwc/companyInvoices/companyInvoices.js
+++ force-app/main/default/lwc/companyInvoices/companyInvoices.js
@@ -1,9 +1,7 @@
 import { LightningElement, wire, api } from 'lwc';
 import ShowInvoices from '@salesforce/apex/CustomerDetailsController.ShowInvoices';
-import CUSTOMER_FIELD from '@salesforce/schema/Invoice__c.Customer__r.FirstName';
-import COMPANY_FIELD from '@salesforce/schema/Invoice__c.Company__c';
 import Status_FIELD from '@salesforce/schema/Invoice__c.Status__c';
-import PAID_DATE from '@salesforce/schema/Invoice__c.Paid_date__c';
+import INVOICE_DATE from '@salesforce/schema/Invoice__c.Invoice_Date__c';
 import Line_AMOUNT_FIELD from '@salesforce/schema/Invoice__c.Total_Line_Amount__c';
 import DUE_DATE from '@salesforce/schema/Invoice__c.Due_Date__c';
 import INVOICE_NO_FIELD from '@salesforce/schema/Invoice__c.Name';
@@ -26,8 +24,8 @@ const COLS = [{
         fieldName: Line_AMOUNT_FIELD.fieldApiName
     },
     {
-        label: 'Paid Date',
-        fieldName: PAID_DATE.fieldApiName
+        label: 'Invoice Date',
+        fieldName: INVOICE_DATE.fieldApiName
     }, {
         label: 'Due Date',
         fieldName: DUE_DATE.fieldApiName
@@ -44,14 +42,18 @@ export default class CompanyInvoices extends LightningElement {
     @wire(ShowInvoices, { companyId: '$recordId' })
     wiredInvoices({ data, error }) {
         if (data) {
-            console.log(data);
-            this.invoiceData = data.map((invoice) => {
-                const customerName = invoice.Customer__r ? `${invoice.Customer__r.FirstName} ${invoice.Customer__r.LastName}` : 'N/A';
-                return {
-                    ...invoice,
-                    CustomerName: customerName
-                }
-            });
+            console.log("**", data);
+            if (data.length == 0) {
+                this.invoiceData = null;
+            } else {
+                this.invoiceData = data.map((invoice) => {
+                    const customerName = invoice.Customer__r ? `${invoice.Customer__r.FirstName} ${invoice.Customer__r.LastName}` : 'N/A';
+                    return {
+                        ...invoice,
+                        CustomerName: customerName
+                    }
+                });
+            }
             //console.log(JSON.stringify(this.invoiceData));
         }
         if (error) {
