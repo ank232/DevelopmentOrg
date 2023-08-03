e4c15e42662261d0cd60fcb6b6347b0f4088e511
Ankur Trivedi <anktridev@gmail.com>
2023-08-03 17:14:51 +0530

    working on a solution to fetch updated inv status

--- force-app/main/default/lwc/invoiceButtons/invoiceButtons.js
+++ force-app/main/default/lwc/invoiceButtons/invoiceButtons.js
@@ -1,11 +1,16 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import { MessageContext, subscribe } from 'lightning/messageService';
+import { getRecord } from 'lightning/uiRecordApi';
+import Stauts_field from '@salesforce/schema/Invoice__c.Status__c';
 import InvoiceTotalMC from '@salesforce/messageChannel/InvoiceTotalMC__c';
 export default class InvoiceButtons extends LightningElement {
+    @api recordId;
     connectedCallback() {
         console.log("I am running!!");
         this.LineItemReciever();
     }
+    @wire(getRecord, { recordId: '$recordId', fields: Stauts_field })
+    getInvoiceDetails;
 
     @wire(MessageContext)
     messageContext;
@@ -14,20 +19,27 @@ export default class InvoiceButtons extends LightningElement {
         subscribe(this.messageContext, InvoiceTotalMC, (message) => {
             console.log('Recieved');
             this.InvoiceButtonVisibility(message);
+            console.log("-=>\n", this.getInvoiceDetails);
         });
     }
+
     InvoiceButtonVisibility(data) {
         const button = this.template.querySelector(".paymentButton");
         if (data.length == 0) {
+            console.log('data has no lineItems');
             button.classList.add('slds-hide');
         } else {
-            button.classList.replace("slds-hide", "slds-show");
+            console.log('Data has linesItems??');
+            console.log(data);
+            button.classList.remove("slds-hide");
         }
     }
+
     RecordPayment = () => {
-
+        console.log("I will record the payment");
     }
-    RecordRefund = () => {
 
+    RecordRefund = () => {
+        console.log('I will record the refund if any!!');
     }
 }
