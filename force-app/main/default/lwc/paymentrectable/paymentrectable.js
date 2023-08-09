6141ef306e01c94dc49f324c81c80cf582cca166
Ankur Trivedi <anktridev@gmail.com>
2023-08-09 22:51:04 +0530

    added payment delete functionality

--- force-app/main/default/lwc/paymentrectable/paymentrectable.js
+++ force-app/main/default/lwc/paymentrectable/paymentrectable.js
@@ -1,12 +1,28 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { deleteRecord } from 'lightning/uiRecordApi';
 export default class Paymentrectable extends LightningElement {
     @api recordId;
     paymentrecs;
+
+    showNoficiation(title, message, variant) {
+        const showToast = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(showToast);
+    }
+
     @wire(getRelatedListRecords, {
         parentRecordId: '$recordId',
         relatedListId: 'Payments__r',
-        fields: ['Payment__c.Name', 'Payment__c.Reference_Number__c', 'Payment__c.Amount__c', 'Payment__c.Status__c', 'Payment__c.Comments__c', 'Payment__c.Date__c']
+        fields: ['Payment__c.Id',
+            'Payment__c.Name',
+            'Payment__c.Reference_Number__c',
+            'Payment__c.Amount__c', 'Payment__c.Status__c', 'Payment__c.Comments__c', 'Payment__c.Date__c'
+        ]
     })
     wiredPaymentRecords({ data, error }) {
         if (data) {
@@ -14,6 +30,7 @@ export default class Paymentrectable extends LightningElement {
             const fieldVals = data.records;
             fieldVals.forEach(obj => {
                 let record = {};
+                record.Id = obj.fields.Id.value;
                 record.Reference = obj.fields.Reference_Number__c.value;
                 record.Date = obj.fields.Date__c.value;
                 record.Status__c = obj.fields.Status__c.value;
@@ -23,13 +40,25 @@ export default class Paymentrectable extends LightningElement {
                 temprecs.push(record);
             });
             this.paymentrecs = temprecs;
-            console.log('Below');
-            console.log(JSON.stringify(this.paymentrecs));
         }
         if (error) {
             console.log("Error at relatedRecs!!");
             console.log(error);
             this.paymentrecs = null;
         }
+    }
+    deletePaymentRecord = (event) => {
+        const rowIndex = event.target.dataset.id;
+        const recTodelete = this.paymentrecs[rowIndex];
+        this.deletePayment(recTodelete.Id, rowIndex);
+    }
+    deletePayment(paymentId, rowid) {
+        deleteRecord(paymentId).then(() => {
+            this.paymentrecs.splice(rowid, 1);
+            this.paymentrecs = [...this.paymentrecs];
+            this.showNoficiation("Message", "Payment has been deleted", "Message");
+        }).catch((error) => {
+            this.showNoficiation("Error", error.message, "Error");
+        });
     }
 }
