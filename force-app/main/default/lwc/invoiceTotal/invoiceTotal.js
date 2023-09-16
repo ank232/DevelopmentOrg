37de9d9d4e6dae79b431d23fbad8f065b2459d46
Ankur Trivedi <anktridev@gmail.com>
2023-09-16 19:20:23 +0530

    updated invoiceTotal component

--- force-app/main/default/lwc/invoiceTotal/invoiceTotal.js
+++ force-app/main/default/lwc/invoiceTotal/invoiceTotal.js
@@ -20,43 +20,58 @@ export default class InvoiceTotal extends LightningElement {
         this.showConvertedAmount();
       })
       .catch(error => {
-        console.log("Failure!");
-        console.log(error);
+        console.log(JSON.stringify(error));
+        this.exchangerate = '$';        
       });
   }
   connectedCallback() {
     this.MessageReciever();
   }
   MessageReciever() {
-    subscribe(this.messageContext, InvoiceTotalMC, message => {
-      console.log("Message recieved in Inv Total!!");
-      console.log(message);
+    subscribe(this.messageContext, InvoiceTotalMC, message => {      
       this.currencytype = message.invoiceCurrencyCode;
       this.fetchrates(message.invoiceCurrencyCode);
       this.invoiceLines = message.invoicelines;
     });
   }
-  showConvertedAmount() {
-    console.log("Current rate == ", this.exchangerate);
+  showConvertedAmount() {    
     const exchange = this.exchangerate;
-    console.log("Lines are---");
-    console.log(this.invoiceLines);
     this.InvoiceTotal(this.invoiceLines, exchange);
   }
   InvoiceTotal(data, clientCurrency) {
-    const taxTotal = invoiceLineItems.reduce(
-      (total, lineItems) => total + parseFloat(lineItems.taxAmount),
+    console.log(clientCurrency);
+    const taxTotal = data.reduce(
+      (total, lineItems) => total + this.desctructcurrency(lineItems.taxAmount),
       0.0
     );
-    const subtotal = invoiceLineItems.reduce(
-      (total, lineItems) => total + parseFloat(lineItems.totalAmount),
-      0.0
-    );
+    data.forEach((lineItem) => {
+      if (typeof lineItem.totalAmount === 'string') {
+        lineItem.totalAmount = parseFloat(lineItem.totalAmount);
+      }
+    });
+    // this.convertedgrandtotal = this.convertedsubtotal + this.convertedt
+    const subtotal = data.reduce(
+      (total, lineItem) => total + lineItem.totalAmount, 0.00);
     this.invtax = taxTotal;
     this.invsubtotal = subtotal;
     this.invgrandtotal = taxTotal + subtotal;
-    this.convertedsubtotal = this.invsubtotal * clientCurrency;
-    this.convertedtax = this.invtax * clientCurrency;
-    this.convertedgrandtotal = this.convertedsubtotal + this.convertedtax;
+    // this.convertedsubtotal = this.invsubtotal * clientCurrency;
+    // this.convertedtax = this.invtax * clientCurrency;ax;
+  }
+
+  desctructcurrency(value) {
+    const hasCurrencySymbol = /[^\d.,]/.test(value);
+    if (hasCurrencySymbol) {
+      // Remove non-numeric characters except for periods and commas
+      const numericPart = value.replace(/[^\d.,]/g, '');
+      // Replace commas with empty strings
+      const numericValue = numericPart.replace(/,/g, '');
+      // Parse the numeric value as a floating-point number
+      return parseFloat(numericValue);
+    } else {
+      // The value does not contain a currency symbol, so it's a currency code
+      return parseFloat(value) || 0; // Convert the value to a number, or return 0 if it's not a valid number
+    }
+    // return parseFloat(value.replace(/[^0-9,-]/g, ""));
   }
 }
